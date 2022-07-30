8c43cb737b178c8fb27c36a0a77a371ec6a4f388
NagarajuPalepu22 <107348585+NagarajuPalepu22@users.noreply.github.com>
2022-07-30 16:52:11 +0530

    my first commite

---
+++ src/main/java/com/SportySheos/model/SportySheosApplication.java
@@ -0,0 +1,13 @@
+package com.SportySheos.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportySheosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportySheosApplication.class, args);
+	}
+
+}
